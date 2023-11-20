8bb3dc2b7eab015b12d3d33d521eee7ed519af42
Jjunior112 <juarezgcjr@gmail.com>
2023-11-19 21:46:21 -0300

    alteração das cores dos icones

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -53,11 +53,11 @@ const Profile = () => {
 
             <motion.div initial={{ x: 100 }} animate={{ x: 0 }} transition={{ duration: 1.5 }}>
 
-                <a href="https://www.linkedin.com/in/juarez-gonzaga-de-castro-jnr" target="_blank" title='Link para perfil do Linkedin' rel="noopener noreferrer"><FaLinkedin /></a>
+                <a href="https://www.linkedin.com/in/juarez-gonzaga-de-castro-jnr" target="_blank" title='Link para perfil do Linkedin' rel="noopener noreferrer"><FaLinkedin className='linkedin' /></a>
 
-                <a href="https://github.com/Jjunior112" target="_blank" title='Link para perfil do GitHub' rel="noopener noreferrer"><FaGithub /></a>
+                <a href="https://github.com/Jjunior112" target="_blank" title='Link para perfil do GitHub' rel="noopener noreferrer" ><FaGithub className='github' /></a>
 
-                <a href="https://jjunior112.github.io/portfolioDev/Curriculo.pdf" download='curriculo.pdf' title='Link para download do curriculo em pdf' rel="noopener noreferrer"><FaFileDownload /></a>
+                <a href="https://jjunior112.github.io/portfolioDev/Curriculo.pdf" download='curriculo.pdf' title='Link para download do curriculo em pdf' rel="noopener noreferrer"><FaFileDownload className='download' /></a>
 
             </motion.div>
 
